2ef075368b4443736ac97be455ad0153c1ca08d5
Jojo <jfrasier96@gmail.com>
2023-03-06 21:01:04 -0500

    updating project with pull

--- assets/horoscope.js
+++ assets/horoscope.js
@@ -1,28 +1,29 @@
 //required auth options
 const options = {
-	method: 'POST',
-	headers: {
-		'X-RapidAPI-Key': '41f3c93319msh95fc82a3c49c3fcp14cfcfjsnf18e5631110f',
-		'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
-	}
-}
-const signsUrl = "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aquarius&day=today" 
-const horoscopeContainer = document.querySelector('.horoscopeContainer')
+  method: "POST",
+  headers: {
+    "X-RapidAPI-Key": "41f3c93319msh95fc82a3c49c3fcp14cfcfjsnf18e5631110f",
+    "X-RapidAPI-Host": "sameer-kumar-aztro-v1.p.rapidapi.com",
+  },
+};
+const signsUrl =
+  "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aquarius&day=today";
+const horoscopeContainer = document.querySelector(".horoscopeContainer");
 
 //get data
-async function getSignData(){
-    let response = await fetch(signsUrl, options)
-	var data = await response.json()
-    displayData(data)
+async function getSignData() {
+  let response = await fetch(signsUrl, options);
+  var data = await response.json();
+  displayData(data);
 }
 
 //display data
-async function displayData(data){
-    Object.keys(data).forEach((key)=>{
-        let keyEl = document.createElement('p')
-        keyEl.textContent = `${key}: ${data[key]}`
-        horoscopeContainer.append(keyEl)
-    })
+async function displayData(data) {
+  Object.keys(data).forEach((key) => {
+    let keyEl = document.createElement("p");
+    keyEl.textContent = `${key}: ${data[key]}`;
+    horoscopeContainer.append(keyEl);
+  });
 }
 
-getSignData()
+getSignData();
