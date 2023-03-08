e30320a429a2689e86877b83db1a55b4d3940287
Mullens <telecotech82@gmail.com>
2023-03-08 18:02:59 -0500

    Changed the js around to only pick 1 day at a time

--- assets/nasa.js
+++ assets/nasa.js
@@ -1,74 +1,33 @@
-const fetchButton = document.getElementById("fetch-button");
-fetchButton.addEventListener("click", fetchApods);
+const apiKey = "enBTmSmHbWFRpxPzHkEBcyf2xpV5YSBBhXGKkqnj";
+const apiUrl = "https://api.nasa.gov/planetary/apod";
 
-function fetchApods() {
-  const apiKey = "GlSGHq2TnpNyMQJfxZHBDc3q4DLE9E8GR2yVhl4f";
-  const apodUrls = [];
+const form = document.querySelector("form");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const date = document.getElementById("date").value;
+  const queryUrl = `${apiUrl}?api_key=${apiKey}&date=${date}`;
+  fetch(queryUrl)
+    .then(response => response.json())
+    .then(data => {
+      const apodDiv = document.getElementById("apod");
+      apodDiv.innerHTML = ""; // clear previous content
+      const image = document.createElement("img");
+      image.src = data.url;
+      image.alt = "NASA Astronomy Picture of the Day";
+      apodDiv.appendChild(image);
 
-  const startDate = $("#start-date").datepicker("getDate");
-  const endDate = $("#end-date").datepicker("getDate");
+      const title = document.createElement("p");
+      title.textContent = `Title: ${data.title}`;
+      apodDiv.appendChild(title);
 
-  for (let date = new Date(startDate); date <= endDate; date.setDate(date.getDate() + 1)) {
-    const formattedDate = date.toISOString().slice(0, 10);
-    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${formattedDate}`;
-    apodUrls.push(apiUrl);
-  }
-
-  const apodPromises = apodUrls.map(url => fetch(url).then(response => response.json()));
-  Promise.all(apodPromises)
-    .then(apods => {
-      const apodsDiv = document.getElementById("apods");
-      apodsDiv.innerHTML = "";
-
-      apods.forEach(apod => {
-        const title = apod.title;
-        const explanation = apod.explanation;
-        const imageUrl = apod.url;
-        const image = document.createElement("img");
-        image.src = imageUrl;
-
-        const titleElem = document.createElement("h2");
-        titleElem.textContent = title;
-        apodsDiv.appendChild(titleElem);
-
-        const explanationElem = document.createElement("p");
-        explanationElem.textContent = explanation;
-        apodsDiv.appendChild(explanationElem);
-
-        apodsDiv.appendChild(image);
-      });
+      const explanation = document.createElement("p");
+      explanation.textContent = `Explanation: ${data.explanation}`;
+      apodDiv.appendChild(explanation);
     })
-    .catch(error => console.error(error));
-}
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-$(function() {
-  
-  var minDate = new Date(2023, 0, 1);
-  var maxDate = new Date(2023, 12, 30);
 
- 
-  $("#start-date").datepicker({
-    dateFormat: "yy-mm-dd",
-    minDate: minDate,
-    maxDate: maxDate,
-    onSelect: function(selectedDate) {
-    
-      var startDate = $(this).datepicker("getDate");
-      var minEndDate = new Date(startDate.getTime());
-      minEndDate.setDate(startDate.getDate() + 7); 
-      $("#end-date").datepicker("option", "minDate", minEndDate);
-    }
-  });
-  $("#end-date").datepicker({
-    dateFormat: "yy-mm-dd",
-    minDate: minDate,
-    maxDate: maxDate,
-    onSelect: function(selectedDate) {
-      
-      var endDate = $(this).datepicker("getDate");
-      var maxStartDate = new Date(endDate.getTime());
-      maxStartDate.setDate(endDate.getDate() - 7); 
-      $("#start-date").datepicker("option", "maxDate", maxStartDate);
-    }
-  });
-}); 
+
