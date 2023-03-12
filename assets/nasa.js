c6c5d41dbd54e9f916a3f6b701c7d02fbc889e94
Mullens <telecotech82@gmail.com>
2023-03-12 17:04:10 -0400

    made changes to my js files to make it work

--- assets/nasa.js
+++ assets/nasa.js
@@ -1,11 +1,26 @@
-const apodImage = document.getElementById("apod-image");
-const apodTitle = document.getElementById("apod-title");
-const apodDescription = document.getElementById("apod-description");
 
 
-const apiData = JSON.parse(localStorage.getItem('apiData'));
+
+const data = JSON.parse(localStorage.getItem('apiData'));
 
 
-apodImage.src = apiData.url;
-apodTitle.textContent = apiData.title;
-apodDescription.textContent = apiData.explanation;
+
+const dataContainer = document.querySelector('#data-container');
+  
+    const imgElement = document.createElement('img');
+    imgElement.src = data.url;
+    dataContainer.appendChild(imgElement);
+  
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = data.title;
+    dataContainer.appendChild(titleElement);
+  
+    const dateElement = document.createElement('p');
+    dateElement.textContent = `Date: ${data.date}`;
+    dataContainer.appendChild(dateElement);
+  
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = data.explanation;
+    dataContainer.appendChild(descriptionElement);
+
+  
