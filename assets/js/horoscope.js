5309c5209ed5a75c35595839c4d69cba3f7e8853
Dewey Watkins <deweywatkins77@gmail.com>
2023-03-13 21:08:18 -0400

    changed nasa header to allow background picture to show through

---
+++ assets/js/horoscope.js
@@ -0,0 +1,83 @@
+//required auth options
+const options = {
+  method: "POST",
+  headers: {
+    "X-RapidAPI-Key": "41f3c93319msh95fc82a3c49c3fcp14cfcfjsnf18e5631110f",
+    "X-RapidAPI-Host": "sameer-kumar-aztro-v1.p.rapidapi.com",
+  },
+};
+const signsUrl = "https://sameer-kumar-aztro-v1.p.rapidapi.com/";
+const horoscopeContainer = document.querySelector(".horoscopeContainer");
+const signh2El = document.querySelector(".signh2");
+const tomorrowEl = document.querySelector(".tomorrow");
+const todayEl = document.querySelector(".today");
+const yesterdayEl = document.querySelector(".yesterday");
+const modalEl = document.querySelector(".signlist");
+const signImg = document.querySelector(".signImg");
+savedSign = JSON.parse(localStorage.getItem("signCache"));
+//setup modal
+$("#dialog").dialog({
+  autoOpen: false,
+  show: {
+    effect: "blind",
+    duration: 500,
+  },
+  hide: {
+    effect: "blind",
+    duration: 250,
+  },
+});
+$("#opener").on("click", function () {
+  $("#dialog").dialog("open");
+});
+//if users sign isnt saved locally, open modal
+if (!savedSign) {
+  //set savedSign in case user closes window and doesn't select a sign.
+  savedSign = { sign: "aquarius" };
+  $("#dialog").dialog("open");
+}
+//eventlistner for modal signs
+modalEl.addEventListener("click", function (event) {
+  selectedSign = event.target;
+  savedSign.sign = selectedSign.textContent;
+  fetchUrl = `${signsUrl}?sign=${savedSign.sign}&day=today`;
+  localStorage.setItem("signCache", JSON.stringify(savedSign));
+  getSignData();
+  $("#dialog").dialog("close");
+});
+fetchUrl = `${signsUrl}?sign=${savedSign.sign}&day=today`;
+//get data
+async function getSignData() {
+  let response = await fetch(fetchUrl, options);
+  var data = await response.json();
+  displayData(data);
+}
+//display data
+async function displayData(data) {
+  signImg.src = `./assets/images/${savedSign.sign}.jpg`;
+  signh2El.textContent = savedSign.sign;
+  signDescription = document.querySelector(".description");
+  signDescription.textContent = data.description;
+  signDate = document.querySelector(".daterange");
+  signDate.textContent = data.date_range;
+  signLuckynumber = document.querySelector(".luckynumber");
+  signLuckynumber.textContent = `Lucky Number: ${data.lucky_number}`;
+  signmood = document.querySelector(".mood");
+  signmood.textContent = `Mood: ${data.mood}`;
+}
+//function for changing url and fetching for day event listners
+function changeDay(day) {
+  fetchUrl = `${signsUrl}?sign=${savedSign.sign}&day=${day}`;
+  getSignData();
+}
+//eventlistners for day buttons
+yesterdayEl.addEventListener("click", function () {
+  changeDay("yesterday");
+});
+todayEl.addEventListener("click", function () {
+  changeDay("today");
+});
+tomorrowEl.addEventListener("click", function () {
+  changeDay("tomorrow");
+});
+getSignData();
---
+++ assets/js/nasa.js
@@ -0,0 +1,18 @@
+const data = JSON.parse(localStorage.getItem('apiData'));
+const dataContainer = document.querySelector('#data-container');
+  
+const imgElement = document.createElement('img');
+imgElement.src = data.url;
+dataContainer.appendChild(imgElement);
+
+const titleElement = document.createElement('h2');
+titleElement.textContent = data.title;
+dataContainer.appendChild(titleElement);
+
+const dateElement = document.createElement('p');
+dateElement.textContent = `Date: ${data.date}`;
+dataContainer.appendChild(dateElement);
+
+const descriptionElement = document.createElement('p');
+descriptionElement.textContent = data.explanation;
+dataContainer.appendChild(descriptionElement);
